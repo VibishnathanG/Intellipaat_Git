16b968eec3618be1aaff75b91f1a22e1c227aabe
vibish <vibishdevops@gmail.com>
2026-02-07 00:31:47 +0530

    NPM update fix

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,9 +1,9 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const exec = require('@actions/exec');
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import * as exec from '@actions/exec';
 
 function run() {
-    core.notice("Hello  from JS HRU!")
+    core.notice("Hello from JS HRU!");
 }
 
-run()
+run();

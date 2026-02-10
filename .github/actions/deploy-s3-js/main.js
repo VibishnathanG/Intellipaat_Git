4eae7f988edefa8c66825376e20bc1db3d80d1d6
vibish <vibishdevops@gmail.com>
2026-02-11 00:19:13 +0530

    ci skip

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -3,7 +3,17 @@ import * as github from '@actions/github';
 import * as exec from '@actions/exec';
 
 function run() {
-    core.notice("Hello from JS HRU!");
+
+    //1) Get Inputs
+    const bucketName = core.getInput('bucket', { required: true });
+    const artifactPath = core.getInput('dist-folder', { required: true });
+    const region = core.getInput('bucket-region');
+
+    //2) Upload the artifacts to S3
+    exec.exec(`aws s3 cp ${artifactPath} s3://${bucketName}/ --recursive --region ${region}`);
+
+    //3) Log the upload process
+    core.notice(`Uploading artifacts from ${artifactPath} to S3 bucket ${bucketName} in region ${region}`);
 }
 
 run();

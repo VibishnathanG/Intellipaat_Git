14581f090b9e4561aa724af6117414d7e4a6d094
vibish <vibishdevops@gmail.com>
2026-02-19 01:11:48 +0530

    Bug Fix

--- .github/actions/deploy-s3-docker/deployment.py
+++ .github/actions/deploy-s3-docker/deployment.py
@@ -1,20 +1,19 @@
 import os
 import boto3
-from botocore.config import Config
 
 def run():
     bucket = os.environ['INPUT_BUCKET']
     bucket_region = os.environ['INPUT_BUCKET_REGION']
     dist_folder = os.environ['INPUT_DIST_FOLDER']
-
-    configuration = Config(region_name=bucket_region)
-
-    s3_client = boto3.client('s3', config=configuration)
-
+    s3_client = boto3.client('s3', region_name=bucket_region)
     for root, dirs, files in os.walk(dist_folder):
         for file in files:
             file_path = os.path.join(root, file)
             s3_key = os.path.relpath(file_path, dist_folder)
             s3_client.upload_file(file_path, bucket, s3_key)
     website_url = f'http://{bucket}.s3-website-{bucket_region}.amazonaws.com'
-    print(f'::set-output name=website-url::{website_url}')
+    github_output = os.environ.get("GITHUB_OUTPUT")
+    if github_output:
+        with open(github_output, "a") as f:
+            f.write(f"website-url={website_url}\n")
+
